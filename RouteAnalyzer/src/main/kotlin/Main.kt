a93e3d93f60477de876c74a4735f0d18ef6d0bd0
ice-cooooolo <1807024877@qq.com>
2025-03-07 18:52:20 +0100

    Revert "add ignore + getdata"
    This reverts commit 16d57b4d

--- RouteAnalyzer/src/main/kotlin/Main.kt
+++ RouteAnalyzer/src/main/kotlin/Main.kt
@@ -1,49 +1,10 @@
 package org.example
 
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-
-fun get_timestamp(data:List<Array<String>>): List<Double>{
-    val first_string = data.map({ row -> row.joinToString(", ") })
-        .map { row -> row.split(";")[0] }
-    val first_double = first_string.mapNotNull { it.toDoubleOrNull() }
-    return first_double
-}
-
-fun get_data(data:List<Array<String>>): List<Map<String, Any>>{
-    val result = data.map({ row -> row.joinToString(", ") })
-        .map {item ->
-                val parts = item.split(";")
-                mapOf(
-                    "time" to parts[0],
-                    "altitude" to parts[1],
-                    "latitude" to parts[2],
-                )
-        }
-    return result
-}
-
-fun get_json_data(data:List<Array<String>>): List<WayPoint>{
-    val result = data.map({ row -> row.joinToString(";") })
-        .map {item ->
-            val parts = item.split(";")
-            WayPoint(parts[0],parts[0],parts[0])
-
-        }
-    return result
-}
 
 fun main() {
+//    println("Hello World!")
     val rows = CsvTools.ReadCsv("waypoints.csv")
-//    rows.forEach { row ->
-//        println(row.joinToString(", "))
-//    }
-//    val first = rows.map({ row -> row.joinToString(", ") })
-//                    .map { row -> row.split(";")[0] }
-//    first.forEach { s -> println(s) }
-//    val time_stamp = get_timestamp(rows)
-//    time_stamp.forEach(::println)
-    val data = get_json_data(rows)
-    println(Json.encodeToString(data))
-//    data.forEach(::println)
+    rows.forEach { row ->
+        println(row.joinToString(", "))
+    }
 }
