70fdd6ea15ee0f60d5ed860fd537142dd6cda6a8
ice-cooooolo <1807024877@qq.com>
2025-03-07 15:30:48 +0100

    push fucking all

--- RouteAnalyzer/src/main/kotlin/CsvTools.kt
+++ RouteAnalyzer/src/main/kotlin/CsvTools.kt
@@ -1,13 +1,16 @@
 package org.example
 
 import com.opencsv.CSVReader
+import java.io.File
 import java.io.FileReader
 
 
 object CsvTools {
 
     fun ReadCsv(file_name: String): List<Array<String>> {
-        val file = FileReader(file_name)
+        val baseDir = System.getProperty("user.dir") // 获取项目根目录
+        val file_path = File("$baseDir/Resource/$file_name")
+        val file = FileReader(file_path)
         val csvReader = CSVReader(file)
 
         // 读取所有行
--- RouteAnalyzer/src/main/kotlin/Main.kt
+++ RouteAnalyzer/src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ package org.example
 
 fun main() {
 //    println("Hello World!")
-    val rows = CsvTools.ReadCsv("D:\\polito\\web2\\Project\\Resource\\waypoints.csv")
+    val rows = CsvTools.ReadCsv("waypoints.csv")
     rows.forEach { row ->
         println(row.joinToString(", "))
     }
