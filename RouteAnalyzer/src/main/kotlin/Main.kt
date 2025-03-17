ff9ba1062111bb87afed5bb229452739d0c66a6f
kangchenghua <chenghuakang@gmail.com>
2025-03-17 19:57:18 +0100

    feat: add json create

--- RouteAnalyzer/src/main/kotlin/Main.kt
+++ RouteAnalyzer/src/main/kotlin/Main.kt
@@ -1,10 +1,66 @@
 package org.example
 
+import kotlinx.serialization.json.*
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.io.Writer
+import javax.naming.Context
+
+fun maxDistanceFromStart(str:String):String{
+    val data = "x"
+    return  data;
+}
+fun mostFrequentedArea(){}
+fun waypointsOutsideGeofence(){}
 
 fun main() {
 //    println("Hello World!")
     val rows = CsvTools.ReadCsv("waypoints.csv")
+
     rows.forEach { row ->
         println(row.joinToString(", "))
     }
+    val json = buildJsonObject {
+        putJsonObject("maxDistanceFromStart") {
+            putJsonObject("waypoint"){
+                    put("timestamp","number")
+                    put("latitude","number")
+                    put("longitude","number")
+            }
+            put("distanceKm","xx")
+        }
+        putJsonObject("mostFrequentedArea") {
+            putJsonObject("centralWaypoint"){
+                put("timestamp","number")
+                put("latitude","number")
+                put("longitude","number")
+            }
+            put("areaRadiusKm","xx")
+            put("entriesCount","xx")
+        }
+        putJsonObject("waypointsOutsideGeofence") {
+            putJsonObject("centralWaypoint"){
+                put("timestamp","number")
+                put("latitude","number")
+                put("longitude","number")
+            }
+            put("areaRadiusKm","xx")
+            put("count","xx")
+            putJsonArray("waypoints"){
+                for(i in 0 .. 5)add(buildJsonObject {
+                    putJsonObject("centralWaypoint"){
+                        put("timestamp",i)
+                        put("latitude",i)
+                        put("longitude",i)
+                    }})
+            }
+
+        }
+    }
+    val filename = "outputs.json";
+
+    val writer = FileWriter(filename);
+    writer.write(json.toString());
+    writer.close()
 }
